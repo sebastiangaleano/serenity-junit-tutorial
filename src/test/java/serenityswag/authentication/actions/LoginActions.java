f5eb2a4bc16f2bb7e3b952ba7b02e77b5616138a
sebastiangaleano <84464550+sebastiangaleano@users.noreply.github.com>
2021-11-12 12:14:47 -0500

    Cambio de locators por id

--- src/test/java/serenityswag/authentication/actions/LoginActions.java
+++ src/test/java/serenityswag/authentication/actions/LoginActions.java
@@ -1,7 +1,9 @@
 package serenityswag.authentication.actions;
 
+import net.serenitybdd.core.annotations.findby.By;
 import net.serenitybdd.core.steps.UIInteractionSteps;
 import net.thucydides.core.annotations.Step;
+import org.openqa.selenium.support.pagefactory.ByAll;
 import serenityswag.authentication.User;
 
 public class LoginActions extends UIInteractionSteps {
@@ -9,10 +11,13 @@ public class LoginActions extends UIInteractionSteps {
   @Step("Log in as a {0}")
   public void as(User user) {
     openUrl("https://www.saucedemo.com/");
-    $("[data-test='username']").sendKeys(user.getUsername());
-    $("[data-test='password']").sendKeys(user.getPassword());
-    $("[data-test='login-button']").click();
 
+//    $("[data-test='username']").sendKeys(user.getUsername());
+    $("#user-name").sendKeys(user.getUsername());
+//    $("[data-test='password']").sendKeys(user.getPassword());
+    $("#password").sendKeys(user.getPassword());
+//    $("[data-test='login-button']").click();
+    $("#login-button").click();
   }
 
 }
